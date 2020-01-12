7a840a941171a95f0582b3ab37bcb13306327b4c
ErrorX <31070311+Nalox1@users.noreply.github.com>
2020-01-12 17:11:12 +0100

    Add files via upload

---
+++ xAtlast/startX.py
@@ -0,0 +1,14 @@
+def startX(s):
+    pass
+    if len(s) > s:
+        return s
+
+    firstLetter[0]
+    if firstLetter != "x":
+        return firstLetter + startX(s [:1])
+    else:
+
+
+print(startX("rexx"))
+print(startX("felixxxxxx"))
+print(startX("rexxdfsxx"))
---
+++ xAtlast/x.py
@@ -0,0 +1,16 @@
+def endX(s):
+    #base case
+    if len(s) < 2:
+        return s
+
+#rescursive case
+    firstLetters[0]
+    if firstLetter != "x":
+        return firstLetters + endX( s[1:])
+    else:
+        return endX( s[1:]) + firstLetter
+print(endX("a"))
+
+print( endX("xxre")) #rexx
+print(endX("xxhixx")) #hixxxx
+print(endX("xhixhix"))#hihixxx
